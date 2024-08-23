5cdbdf03db4f075e43c059b45843644210cfcffc
Aishwarya Apsingikar <111657361+Aishwarya-10-A@users.noreply.github.com>
2024-08-23 22:54:29 +0530

    Time: 0 ms (100%), Space: 45.1 MB (70.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,7 +2,22 @@ class Solution
 {
     public int missingNumber(int[] nums)
     {
-        int n = nums.length;
+       int n=nums.length;
+       int tsum=n*(n+1)/2;
+       int asum=0;
+       for(int num:nums)
+       {
+           asum+=num;
+       }
+       return tsum-asum;
+        
+    }
+}
+
+
+
+
+/*int n = nums.length;
         int expectedSum = n * (n + 1) / 2;
         int actualSum = 0;
         
@@ -11,8 +26,6 @@ class Solution
             actualSum += num;
         }
         
-        return expectedSum - actualSum;
-    }
-}
+        return expectedSum - actualSum;*/
 
 
